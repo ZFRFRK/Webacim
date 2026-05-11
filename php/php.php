5a2b907e37fd5cb6ac96ea250d138734ac6ff16a
ZFRFRK <rafihilmanibrahim@gmail.com>
2026-05-11 14:20:27 +0700

    first commit

---
+++ php/php.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_registration";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get data from form
+$user = $_POST['username'];
+$email = $_POST['email'];
+// Always hash passwords for security!
+$pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+// SQL to insert data
+$sql = "INSERT INTO users (username, email, password) VALUES ('$user', '$email', '$pass')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
